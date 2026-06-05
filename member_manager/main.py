53090d34bdf53103a9c0dc3a01615c0cd5c56664
DESKTOP-U0L0MJS\Admin <whatever.u.r1103@gmail.com>
2026-06-05 18:52:19 +0900

    feat: 회원 추가 기능

--- member_manager/main.py
+++ member_manager/main.py
@@ -11,8 +11,38 @@ while True:
     print("5. 종료")
 
     menu = input()
-    if menu == "1": 
-        member_manager.add_member()
+    if menu == "1":
+        print("등록할 회원의 정보를 입력하세요.")
+        while True:
+            print("이름:", end=" ")
+            name = input()
+            if member_manager.validate_name(name):
+                break
+            print("잘못된 입력입니다.")
+        while True:
+            print("전화번호(ex: 01012345678):", end=" ")
+            phone = input()
+            if member_manager.validate_phone(phone):
+                break
+            print("잘못된 입력입니다.")
+
+        print("주소:", end=" ")
+        address = input()
+
+        while True:
+            print("구분(ex. 가족, 친구, 기타):", end=" ")
+            type = input()
+            if member_manager.validate_type(type):
+                break
+            print("잘못된 입력입니다.")
+
+        members = {
+            "name" : name,
+            "phone" : phone,
+            "address" : address,
+            "type" : type
+        }
+        member_manager.add_member(members)
     elif menu == "2":
         member_manager.list_members()
     elif menu == "3":
